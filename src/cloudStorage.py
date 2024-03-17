6795f1bf42b9466e85f61049a5d240bac2014d33
Davide Chieregato <davide.chieregato96@gmail.com>
2024-03-17 15:05:16 +0100

    CGS_Asset : Storage Client setup

--- src/cloudStorage.py
+++ src/cloudStorage.py
@@ -1,10 +1,16 @@
 # https://googleapis.github.io/google-cloud-python/latest/storage/index.html
-import logging
+# Client connector
 
-from google.cloud import storage
+import logging
 import os
 from google.cloud import storage
-# import
+
+
+logging.getLogger().setLevel(logging.INFO)
+# logging.basicConfig()
+# logger = logging.getLogger()
+# logger.setLevel(logging.INFO)
+
 def storage_client():
     # service account key JSON path
     key_path = r"C:\Users\ECHIERDF9\OneDrive - NTT DATA EMEAL\Desktop\GCS_Asset\GCS_Asset\src\config\smooth-tesla-413121-a1ac05929582.json"
@@ -13,8 +19,9 @@ def storage_client():
     # client = cs.from_service_account_json(key_path)
     client = storage.Client.from_service_account_json(json_credentials_path=key_path)
 
-    print(client)
     return client
+
+    logging.info("client connected")
 
 
 storage_client()
